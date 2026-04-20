9a5b7333efa0d6a19c8e33354caa814a45f7088f
Вячеслав <Vyacheslav@inbox.ru>
2026-04-20 23:30:21 +0700

    fixed plot

--- plot.py
+++ plot.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 
-bdef plot_data(data):
+def plot_data(data):
+    '''Функция визуализации'''
+
+    if not data:
+        raise ValueError('data не может быть пустым!')
     plt.plot(data)
     plt.show()
